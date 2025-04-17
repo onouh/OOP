e9bfdbbf20c75b280f69a7522e9e5e12d8a6374e
Omar Akram <99609294+lolman9002@users.noreply.github.com>
2025-04-17 21:00:14 +0200

    Add files via upload

--- Room.java
+++ Room.java
@@ -5,34 +5,25 @@ import java.util.Calendar;
 public class Room {
 
     private static int roomID = 0 ;
-    private int roomno;
-    private Calendar availableHours;
+    private int roomNo;
     private int capacity;
     
     
     
     public Room( Calendar availableHours, int capacity) {
-        roomno = roomID;
+        roomNo = roomID;
         roomID++;
-        this.availableHours = availableHours;
         this.capacity = capacity;
     }
     
-    public int getRoomID() {
-        return roomID;
+    public int getRoomNo() {
+        return roomNo;
     }
-    public Calendar getAvailableHours() {
-        return availableHours;
-    }
+    
     public int getCapacity() {
         return capacity;
     }
-    public void setRoomID(int roomID) {
-        this.roomID = roomID;
-    }
-    public void setAvailableHours(Calendar availableHours) {
-        this.availableHours = availableHours;
-    }
+    
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
