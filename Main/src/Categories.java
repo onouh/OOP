13e6c64bd7a94cd63c8f0631e36a4f7dc255e35e
Omar Nouh <onouh7@gmail.com>
2025-04-18 20:29:58 +0200

    commit

--- Main/src/Categories.java
+++ Main/src/Categories.java
@@ -1,5 +1,3 @@
-package com.mycompany.oopproject;
-
 import java.util.Scanner;
 
 public class Categories {
--- Main/src/Event.java
+++ Main/src/Event.java
@@ -36,6 +36,4 @@ public class Event {
     public ArrayList<Attendee> getAttendee(){
         return inEvent;
     }
-
-
 }
