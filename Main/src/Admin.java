4d83f89b3b35b51dcad15f4dbbe2d0c76ee007f0
Omar Akram <99609294+lolman9002@users.noreply.github.com>
2025-04-19 18:28:11 +0200

    Add files via upload
    Signed-off-by: Omar Akram <99609294+lolman9002@users.noreply.github.com>

--- Main/src/Admin.java
+++ Main/src/Admin.java
@@ -1,3 +1,5 @@
+package com.mycompany.oopproject;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -5,9 +7,9 @@ import java.util.InputMismatchException;
      
 
 public class Admin extends Person implements Employee<Categories>{
-    private final Calendar dateOfBirth = Calendar.getInstance() ;
-    private final Calendar workBegin = Calendar.getInstance();
-    private final Calendar workEnd = Calendar.getInstance();
+    private Calendar dateOfBirth = Calendar.getInstance() ;
+    private Calendar workBegin = Calendar.getInstance();
+    private Calendar workEnd = Calendar.getInstance();
     private String role;
     Scanner input = new Scanner(System.in);
     
@@ -18,11 +20,11 @@ public class Admin extends Person implements Employee<Categories>{
           int monthOfBirth, int dayOfBirth, int start, int End) {
         super(username, password);
         this.role = role;
-        this.dateOfBirth.set(Calendar.YEAR, yearOfBirth); // Corrected
-        this.dateOfBirth.set(Calendar.MONTH, monthOfBirth - 1); // Corrected
-        this.dateOfBirth.set(Calendar.DAY_OF_MONTH, dayOfBirth); // Corrected
-        this.workBegin.set(Calendar.HOUR_OF_DAY, start); // Corrected
-        this.workEnd.set(Calendar.HOUR_OF_DAY, End); // Corrected
+        this.dateOfBirth.set(Calendar.YEAR, yearOfBirth); 
+        this.dateOfBirth.set(Calendar.MONTH, monthOfBirth - 1); 
+        this.dateOfBirth.set(Calendar.DAY_OF_MONTH, dayOfBirth); 
+        this.workBegin.set(Calendar.HOUR_OF_DAY, start); 
+        this.workEnd.set(Calendar.HOUR_OF_DAY, End); 
     }
     
     
@@ -32,7 +34,7 @@ public class Admin extends Person implements Employee<Categories>{
 
 
     public int getWorkingHours(){
-        return (workEnd.get(Calendar.HOUR_OF_DAY) - workBegin.get(Calendar.HOUR_OF_DAY));
+        return (workEnd.get(Calendar.HOUR_OF_DAY)-workBegin.get(Calendar.HOUR_OF_DAY));// Corrected
     }
     
     
@@ -79,7 +81,7 @@ public class Admin extends Person implements Employee<Categories>{
         ArrayList<String> attendees = new ArrayList<>();
         for(Person p:Database.people){
             if(p instanceof Attendee){
-                attendees.add(p.username);
+                attendees.add(p.getUsername());// Corrected
             }
         }
         int max1 = Math.max(Database.events.size(),Database.rooms.size());
@@ -95,8 +97,8 @@ public class Admin extends Person implements Employee<Categories>{
     
     @Override
     public String toString(){
-    String adminInfo= "Username: " + this.username + " role: " + this.role +  " Working hours: " + 
-            (workEnd.get(workEnd.HOUR_OF_DAY)-workBegin.get(workBegin.HOUR_OF_DAY));  
+    String adminInfo= "Username: " + this.getUsername() + " role: " + this.role +  " Working hours: " + // Corrected
+            (workEnd.get(Calendar.HOUR_OF_DAY)-workBegin.get(Calendar.HOUR_OF_DAY));  // Corrected
         
     return adminInfo;
     }
@@ -109,9 +111,9 @@ public class Admin extends Person implements Employee<Categories>{
 
     for (Person p : Database.people) {
         if (p instanceof Attendee) {
-            attendees.add(p.username);
+            attendees.add(p.getUsername());// Corrected
         } else if (p instanceof Organizer) {
-            organizers.add(p.username);
+            organizers.add(p.getUsername());// Corrected
         }
     }
     
