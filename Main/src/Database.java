d08a2527728745a82962202aaa6bebffc9e4f376
Omar Nouh <onouh7@gmail.com>
2025-04-19 18:54:36 +0200

    commit 1a2b3c4d5e6f7g8h9i0j

--- Main/src/Database.java
+++ Main/src/Database.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList;
 
-
 public class Database {
-    static int intialCapacity = 1000;
-    public static ArrayList<Person> people = new ArrayList<>(intialCapacity);
-    public static ArrayList<Event> events = new ArrayList<>(intialCapacity);
-    public static ArrayList<Room> rooms = new ArrayList<>(intialCapacity);
-    public static ArrayList<Categories> categories = new ArrayList<>(intialCapacity);
+    static final int INITIAL_CAPACITY = 1000;
+    public static ArrayList<Person> people = new ArrayList<>(INITIAL_CAPACITY);
+    public static ArrayList<Event> events = new ArrayList<>(INITIAL_CAPACITY);
+    public static ArrayList<Room> rooms = new ArrayList<>(INITIAL_CAPACITY);
+    public static ArrayList<Categories> categories = new ArrayList<>(INITIAL_CAPACITY);
 }
