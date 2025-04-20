260da1a00ef60f0aed5d6eae605f0db50f589549
Omar Nouh <onouh7@gmail.com>
2025-04-20 17:07:13 +0200

    commit

--- Main/src/Reservations.java
+++ Main/src/Reservations.java
@@ -1,12 +1,14 @@
-package com.mycompany.curroop;
 import java.util.Calendar;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Reservations {
-    private Calendar Reservationtime=Calendar.getInstance();
-    boolean nightAvailable ;
-    boolean dayAvailable ;
+    private Calendar Reservationtime = Calendar.getInstance();
+    boolean nightAvailable;
+    boolean dayAvailable;
     static Scanner input = new Scanner(System.in);
+
     public Reservations() {
     }
 
@@ -14,38 +16,38 @@ public class Reservations {
         this.nightAvailable = true;
         this.dayAvailable = true;
     }
-    
-    public void reserve(Room room,Calendar reservetime , String choice){
+
+    public void reserve(Room room, Calendar reservetime, String choice) {
         this.Reservationtime = reservetime;
         System.out.println("please enter 1 for morning and 2 for night reservation");
-        boolean continueInput =true;
-        do{
+        boolean continueInput = true;
+        do {
             choice = input.nextLine();
-                switch(choice){
-                    case "1" : 
-                        this.dayAvailable = false;
-                        continueInput = false;
-                        break;
-                    case "2" : 
-                        this.nightAvailable = false;
-                        continueInput = false;
-                        break;
-                    default: 
-                        System.out.println("proper input must be either 1 or 2");
-                        break;
-                }
-        }while(continueInput);
+            switch (choice) {
+                case "1":
+                    this.dayAvailable = false;
+                    continueInput = false;
+                    break;
+                case "2":
+                    this.nightAvailable = false;
+                    continueInput = false;
+                    break;
+                default:
+                    System.out.println("proper input must be either 1 or 2");
+                    break;
+            }
+        } while (continueInput);
         room.getUnavalabledates().add(this);
     }
-    
+
     public boolean getDayAvalability() {
         return this.dayAvailable;
     }
-    
+
     public boolean getnightAvalability() {
         return this.nightAvailable;
     }
-    
+
     public Calendar getReservationTime() {
         return this.Reservationtime;
     }
