c43ffa71ad4a1ec6d73a1434aa6e9628b69268ea
OMZaky <24P0117@eng.asu.edu.eg>
2025-04-22 22:41:58 +0200

    Commit

--- Main/src/App.java
+++ Main/src/App.java
@@ -26,7 +26,7 @@ public class App {
 
     Attendee attendee1  = new Attendee(new Wallet(25000), Gender.MALE, "Nasr City", new ArrayList<>(List.of("Birthdays", "Football", "Tv Shows")), "Mohamed", "MyPassword", 1, 1, 2000 );
     Attendee attendee2 = new Attendee(new Wallet(20000), Gender.FEMALE, "Tagamo3", new ArrayList<>(List.of("Wedding", "Movies", "Skating")), "Mariam", "MyPassword", 2, 2, 2000 );
-    Attendee attendee3 = new Attendee(new Wallet(15000), Gender.MALE, "Rehab", new ArrayList<>(List.of("Parties", "Snorkling", "Bachelor Party")), "Omar", "MyPassword", 5, 5, 2000 );
+    Attendee attendee3 = new Attendee(new Wallet(15000), Gender.MALE, "Rehab", new ArrayList<>(List.of("Parties", "Snorkeling", "Bachelor Party")), "Omar", "MyPassword", 5, 5, 2000 );
 
     Room room1 = new Room(100);
     Room room2 = new Room(60);
@@ -46,6 +46,9 @@ public class App {
 
     {
         Database.people.addAll(Arrays.asList(attendee1,attendee2, attendee3, organizer1, organizer2, organizer3));
+        Database.events.addAll(Arrays.asList(event1,event2,event3));
+        Database.rooms.addAll(Arrays.asList(room1,room2,room3));
+        Database.categories.addAll(Arrays.asList(Wedding,Birthday,Conference));
 
     }
 
