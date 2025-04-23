cd0fc9e1b29392c8f092cb14f0ad2d2e1185c017
OMZaky <24P0117@eng.asu.edu.eg>
2025-04-23 20:30:05 +0200

    Commit

--- Main/src/App.java
+++ Main/src/App.java
@@ -37,9 +37,9 @@ public class App {
     static Organizer organizer2 = new Organizer("Farah", "MyPassword", 4, 4, 2000, 40000);
     static Organizer organizer3= new Organizer("Sarah", "MyPassword", 6, 6, 2000, 30000);
 
-    static Event event1 = new Event("Ali's Wedding", Wedding , 1000, calendar1, room1, organizer1);
-    static  Event event2 = new Event("John's Birthday", Birthday , 500, calendar2, room3, organizer2);
-    static Event event3 = new Event("Siemens Conference", Conference , 700, calendar3, room2, organizer3);
+    static Event event1 = new Event("Ali's Wedding", Wedding , 1000, calendar1, room1, organizer1, "Morning");
+    static  Event event2 = new Event("John's Birthday", Birthday , 500, calendar2, room3, organizer2, "Morning");
+    static Event event3 = new Event("Siemens Conference", Conference , 700, calendar3, room2, organizer3, "Morning");
 
     static {
         Database.people.addAll(Arrays.asList(attendee1,attendee2, attendee3, organizer1, organizer2, organizer3));
