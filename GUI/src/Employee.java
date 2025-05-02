d55cf9891ab1e3da80ed1e9491fabf0d2f7f7013
Omar Nouh <onouh7@gmail.com>
2025-05-02 18:38:48 +0300

    commit

--- GUI/src/Employee.java
+++
@@ -1,7 +0,0 @@
-public interface Employee <T> {
-        public void create();
-        public void read(T o);
-        public void update(T o);
-        public void delete(T o);
-        public void show();
-}
