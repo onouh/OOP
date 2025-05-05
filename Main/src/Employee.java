d761437834f6d3788d0f0981749f91ba234c2ac2
Omar Akram <99609294+lolman9002@users.noreply.github.com>
2025-05-05 21:27:33 +0300

    Add files via upload
    Signed-off-by: Omar Akram <99609294+lolman9002@users.noreply.github.com>

--- Main/src/Employee.java
+++ Main/src/Employee.java
@@ -1,8 +1,12 @@
 
+package com.example.app_gui;
+
+import javafx.scene.layout.VBox;
+
 public interface Employee <T> {
-        public void create();
-        public void read(T o);
-        public void update(T o);
+        public void create(String jack , VBox john);
+        public String read(T o);
+        public void update(T o , String newValue , VBox theInputOfTheNewValue);
         public void delete(T o);
         public void show();
 }
